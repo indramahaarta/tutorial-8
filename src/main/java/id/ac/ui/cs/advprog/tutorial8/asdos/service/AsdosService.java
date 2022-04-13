a1057a33ff8f49949d2529bb6e2d9c78a2a9c54c
rheznandyae <rheznandya.erwanto@gmail.com>
2022-04-14 00:38:38 +0700

    Initial Commit

---
+++ src/main/java/id/ac/ui/cs/advprog/tutorial8/asdos/service/AsdosService.java
@@ -0,0 +1,5 @@
+package id.ac.ui.cs.advprog.tutorial8.asdos.service;
+
+public interface AsdosService {
+
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/tutorial8/asdos/service/AsdosServiceImpl.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.tutorial8.asdos.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AsdosServiceImpl implements AsdosService{
+
+
+}
